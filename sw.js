5e7f37bf292a2974d7298b50d7757b9207c33e8a
Javier QUERO <100156062+javierquero@users.noreply.github.com>
2026-08-29 18:59:44 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,69 +6,7 @@ const ASSETS = [
   './icon-192.png',
   './icon-512.png',
   './caja-clue-simpsons.png',
-  './Homer_Simpson_Revised.ttf',
-  'https://cdn.tailwindcss.com'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
-  );
-});const CACHE_NAME = 'clue-simpsons-v33';
-const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './dona.png',
-  './dona-icon.png',
-  './caja-clue-simpsons.png',
-  './Homer_Simpson_Revised.ttf',
-  'https://cdn.tailwindcss.com'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
-  );
-});const CACHE_NAME = 'clue-simpsons-v33';
-const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './dona.png',
-  './caja-clue-simpsons.png',
-  './Homer_Simpson_Revised.ttf',
-  'https://cdn.tailwindcss.com'
+  './Homer_Simpson_Revised.ttf'
 ];
 
 self.addEventListener('install', e => {
